beab64da8530bb8e9af9439dbe01780ca60c4f6d
Rahul Karuvadath <rahulraghav093@gmail.com>
2021-08-27 15:21:59 +0530

    database connection modified

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 module.exports.connect = function (done) {
     // const url='mongodb://localhost:27017'
-    const dbname = 'TrendZStation'
+    const dbname = 'trendzstation'
 
     mongoClient.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, data) => {
         if (err) return done(err)
